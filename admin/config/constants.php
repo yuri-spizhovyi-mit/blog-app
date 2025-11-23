9b585fa6dcbead9c159ae73459ee87c2d665836c
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-11-23 15:30:34 -0800

    Remove sensitive constants from repo

--- admin/config/constants.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-define('ROOT_URL', 'http://localhost/blog-app/'); 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');        // XAMPP default
-define('DB_PASS', '');            // XAMPP default
-define('DB_NAME', 'blog_app');    // Use THIS! not myblog
